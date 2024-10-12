23c94a05a13ef2e859da7c11d78909e6a36e90a8
acsG4briel <gabriel.araujocsilv@gmail.com>
2024-10-12 14:38:20 -0300

    FEAT #0001 - Configurações iniciais back end

---
+++ Lembretes.Api/Controllers/LembreteController.cs
@@ -0,0 +1,59 @@
+﻿using Lembretes.Api.Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lembretes.Api.Controllers
+{
+    [ApiController]
+    [Route("lembretes/")]
+    public class LembreteController : ControllerBase
+    {
+        private List<Lembrete> bancoDeDados = new List<Lembrete>()
+        {
+                new()
+                {
+                    Id = 1,
+                    Data = DateTime.Today.AddDays(1),
+                    Mensagem = "TESTE 1",
+                },
+                new()
+                {
+                    Id = 2,
+                    Data = DateTime.Today.AddDays(2),
+                    Mensagem = "TESTE 2",
+                },
+                new()
+                {
+                    Id = 3,
+                    Data = DateTime.Today.AddDays(2),
+                    Mensagem = "TESTE 3",
+                }
+        };
+
+        public LembreteController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult ExibirLembretes()
+        {
+            return Ok(bancoDeDados);
+        }
+
+        [HttpPost]
+        public IActionResult CadastrarLembrete([FromBody]Lembrete novoLembrete)
+        {
+            bancoDeDados.Add(novoLembrete);
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult ExcluirLembrete([FromQuery] int id)
+        {
+            var lembrete = bancoDeDados.Find(l => l.Id == id);
+            bancoDeDados.Remove(lembrete);
+
+            return Ok();
+        }
+    }
+}
---
+++ Lembretes.Api/Entidades/Lembrete.cs
@@ -0,0 +1,10 @@
+﻿namespace Lembretes.Api.Entidades
+{
+    public class Lembrete
+    {
+        public int Id { get; set; }
+        public string Mensagem { get; set; } = string.Empty;
+        public DateTime Data { get; set; }
+
+    }
+}
